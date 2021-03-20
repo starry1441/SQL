a7d8a8e835549c1a41f35db30a03a1a1e7bf85b5
starry1441 <3363654551@qq.com>
2021-03-20 16:11:47 +0800

    3_20

---
+++ src/template/Add.java
@@ -0,0 +1,61 @@
+package template;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:插入
+ * User: starry
+ * Date: 2021 -03 -20
+ * Time: 15:46
+ */
+public class Add {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        //1. 加载JDBC驱动程序：反射，这样调用初始化com.mysql.jdbc.Driver类，即将该类加载到JVM方法区，并执行该类的静态方法块、静态属性。
+        Class.forName("com.mysql.jdbc.Driver");
+
+        //2. 获取连接
+        String url = "jdbc:mysql://127.0.0.1:3306/ebook?useSSL=false"; //地址
+        //url中  jdbc表示协议   127.0.0.1表示IP地址，默认是本地（也可以写成localhost）   3306表示端口号   ebook表示数据库的名称（需要更换）
+        //IP用来确定主机的位置   端口号用来唯一确定一个“软件”
+        String username = "root";   //用户名,默认是root
+        String password = "111111"; //密码，mysql的密码
+        //Connection connection = DriverManager.getConnection(url,username,password);
+
+        //数据源
+        //改变连接的方式，使用连接池，dataSource是接口
+        DataSource dataSource = new MysqlDataSource();  //MysqlDataSource()不具备连接池的概念,之后可以用c3p0连接池，druid连接池等
+        ((MysqlDataSource)dataSource).setURL(url);
+        ((MysqlDataSource)dataSource).setUser(username);
+        ((MysqlDataSource)dataSource).setPassword(password);
+        Connection connection = dataSource.getConnection();
+
+        //3. 执行SQL语句
+        String sql = "insert into book(name,author,price,category_id) values(?,?,?,?)";    //?表示占位符
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql); //有预编译的功能，优于statement
+        preparedStatement.setString(1,"数据库应用");
+        preparedStatement.setString(2,"星星");
+        preparedStatement.setFloat(3,32.65f);
+        preparedStatement.setInt(4,3);
+
+        //除了 查询 其他都要用executeUpdate，返回值是受影响的行数
+        int ret = preparedStatement.executeUpdate();
+        if(ret != 0) {
+            System.out.println("插入成功");
+        }
+
+        //资源需要关闭
+        preparedStatement.close();
+        connection.close();
+
+    }
+
+}
---
+++ src/template/Book.java
@@ -0,0 +1,68 @@
+package template;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: starry
+ * Date: 2021 -03 -20
+ * Time: 14:57
+ */
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private float price;
+    private int category_id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", price=" + price +
+                ", category_id=" + category_id +
+                '}';
+    }
+
+}
